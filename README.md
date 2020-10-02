8f1e0d2176061131cf455b25a2c440f0a6dfb48a
Hugo Cullinan <hugo.cullinan@hotmail.com>
2020-10-02 11:28:11 +1000

    managed to create new bug

---
+++ README.md
@@ -0,0 +1,44 @@
+# This is a heading
+
+## This is a subheading
+
+### Subsub and so on
+
+*italics*
+
+**strong**
+
+***bolditalics***
+
+list
+---
+
+- yew
+1. yew
+* yew
+
+## hyperlinks
+
+[Here is a link](http://w3c.org)
+
+
+## images
+
+![Kitten picture](http://placekitten.com/120/120)
+
+
+`back-ticks` makes it look like code.
+
+```
+const example = function () {
+  console.log('wowww');
+}
+```
+
+```javascript
+const example = function () {
+  console.log('wowww');
+}
+```
+
+> Block quotes
--- js/index.js
+++ js/index.js
@@ -1,74 +1,132 @@
-// markers (X,O)
-//player one chooses marker
-//random choice for who goes first
-//change marker function at the end of each turn
-
-const markers = ['X','O'];
-const player1Marker = 'X';
-const player2Marker = 'O';
+let player1Marker = '';
+let player2Marker = '';
 let player1 = null;
-let playing = true;
 let tiles = ['','','','','','','','',''];
+let swordScore = 0;
+let shieldScore = 0;
 
-// const checkForWin = function() {
-//   if (tiles[0] && tiles[1] && tiles[2] === )
-// }
-//checks if all
+
+const checkForWin = function (marker) {
+  if (tiles[0] === marker && tiles[1] === marker && tiles[2] === marker ||
+      tiles[3] === marker && tiles[4] === marker && tiles[5] === marker ||
+      tiles[6] === marker && tiles[7] === marker && tiles[8] === marker ||
+      tiles[0] === marker && tiles[3] === marker && tiles[6] === marker ||
+      tiles[1] === marker && tiles[4] === marker && tiles[7] === marker ||
+      tiles[2] === marker && tiles[5] === marker && tiles[8] === marker ||
+      tiles[0] === marker && tiles[4] === marker && tiles[8] === marker ||
+      tiles[2] === marker && tiles[4] === marker && tiles[6] === marker) {
+
+      if (marker === 'X') {
+        $('.Xtile').css('transform','scale(1.4)');
+        $('.box').css('pointer-events', 'none');
+        swordScore += 1;
+        $('#sc1').html(swordScore);
+
+      } else {
+        $('.Otile').css('transform','scale(1.4)');
+        $('.box').css('pointer-events', 'none');
+        shieldScore += 1;
+        $('#sc2').html(shieldScore);
+      }
+
+  }
+
+}
+
+//checks if all tiles are filled and no winner.
 const checkForTie = function() {
-  if (!tiles.includes('')) {
+  if (tiles.includes('')) {
+    playing = true;
+  } else {
     playing = false;
   }
 };
 
 //changes whos turn it is
 const changePlayer = function() {
-  if (player1) {
+  if (player1 === true) {
     player1 = false;
+
   } else {
     player1 = true;
   }
 };
-
 
 //coin toss for which player goes first
 const goesFirst = function() {
   const coin = Math.round(Math.random());
   if (coin === 1) {
     player1 = true;
+
   } else {
     player1 = false;
   };
 };
 
-// click return which box it is.
 $(document).ready(function() {
-  if (playing) {
 
+    $('.bigbtn').on('click', function() {
+
+      $('#shield').toggleClass('shrink');
+      $('#shield').css('pointer-events', 'none');
+      $('#sword').animate({
+        left: -250 ,
+        top: -15,
+      }, 1500);
+      $('#sword').css('pointer-events', 'none');
+      $('#sword').toggleClass('shrink');
+      $('#shield').animate({
+        right: -250 ,
+        top: -15,
+      }, 1500);
+      $('.choose').fadeOut(2000);
+      // $('.choose').css('visibility', 'hidden');
+      if ($(this).attr('id') === 'sword') {
+        player1Marker = 'X'
+        $('#sw').html('P1:')
+        player2Marker = 'O'
+        $('#sh').html('P2:')
+      } else {
+        player1Marker = 'O'
+        $('#sw').html('P2:')
+        player2Marker = 'X'
+        $('#sh').html('P1:')
+      }
+    })
+
+
+    goesFirst()
     $('.box').on('click', function() {
+
       if (player1) {
         const box = $(this).attr('id');
         tiles[parseInt(box)-1] = player1Marker;
-        $(this).html(player1Marker);
+        $(this).addClass(`${ player1Marker }tile`)
         $(this).unbind('click');
-        // checkForWin()
-        checkForTie()
-        changePlayer()
+        checkForTie();
+        changePlayer();
+        checkForWin(player1Marker);
       } else {
         const box = $(this).attr('id');
         tiles[parseInt(box)-1] = player2Marker;
-        $(this).html(player2Marker);
+        $(this).addClass(`${ player2Marker }tile`)
         $(this).unbind('click');
-        // checkForWin()
         checkForTie()
         changePlayer()
-      }
+        checkForWin(player2Marker);
+      };
+
     });
-  } else {
-    console.log("Would you like to play again?");
-  }
+//reset button buggy
+    $('#reset').on('click', function() {
+      tiles = ['','','','','','','','',''];
+
+      $('.box').css("background-image", "url(images/cardfront.png)")
+      $('.box').css('transform','scale(1)');
 
 
 
+    })
 
 
 
